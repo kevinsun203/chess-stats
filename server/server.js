8a8eeece81dadd5bfefd095597712332d1830008
kevinsun203 <kevingrandflyer@gmail.com>
2023-01-30 20:51:03 -0500

    fejo.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require('axios');
 const fsPromises = require("fs").promises;
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
